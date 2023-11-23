e7f7a3ff1d03445d3c8f572b2ab1e8c391b9ce4b
jvianad <jeanvianademares@gmail.com>
2023-11-22 22:23:13 -0500

    Commit 1: Definir entidades, relaciones y estructura

---
+++ src/main/java/com/project/ReDeAmericaBE/ReDeAmericaBeApplication.java
@@ -0,0 +1,13 @@
+package com.project.ReDeAmericaBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReDeAmericaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReDeAmericaBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/ReDeAmericaBE/ReDeAmericaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ReDeAmericaBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReDeAmericaBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
