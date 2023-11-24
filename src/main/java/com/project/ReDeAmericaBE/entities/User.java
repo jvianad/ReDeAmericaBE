3ce87a8fe6c4495683cb7052c70a712bb15522d0
MarianyLopez <marlpez111@misena.edu.co>
2023-11-24 12:49:15 -0500

    Commit 7: Se implementa la interfaz UserDetails en la entidad User

--- src/main/java/com/project/ReDeAmericaBE/entities/User.java
+++ src/main/java/com/project/ReDeAmericaBE/entities/User.java
@@ -3,19 +3,19 @@ package com.project.ReDeAmericaBE.entities;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @Entity
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "user")
-public class User {
+@Table(name = "user",uniqueConstraints = {@UniqueConstraint(columnNames = {"email"})})
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idUser;
@@ -51,5 +51,40 @@ public class User {
         this.role = role;
         this.publications = publications;
         this.comments = comments;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(("ROLE_"+role.name())));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
